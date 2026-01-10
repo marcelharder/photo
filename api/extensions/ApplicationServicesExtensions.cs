9342e222798995283bf499ef6376cc8c625db835
m_harder88@yahoo.com <m_harder88@yahoo.com>
2026-01-10 11:53:11 +0100

    initial commit msi

--- api/extensions/ApplicationServicesExtensions.cs
+++ api/extensions/ApplicationServicesExtensions.cs
@@ -11,16 +11,7 @@ namespace fotoservice.extensions;
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
 
-           
-        //    services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-        //    services.Configure<ComSettings>(config.GetSection("ComSettings"));
-
-           /*  services.AddDbContext<ApplicationDbContext>(options => options
-                       .UseMySql(config.GetConnectionString("SQLConnection"),
-                           mysqlOptions =>
-                               mysqlOptions.ServerVersion(new Pomelo.EntityFrameworkCore.MySql.Storage.ServerVersion(new Version(10, 4, 6), ServerType.MariaDb)).EnableRetryOnFailure()));
-        
-          */ 
+      
       var serverVersion = new MariaDbServerVersion(new Version(8, 0, 34));
           var connectionString = config.GetConnectionString("SQLConnection");
           services.AddDbContext<ApplicationDbContext>(
@@ -36,7 +27,6 @@ namespace fotoservice.extensions;
           
           
             services.AddSingleton<DapperContext>();
-            //services.AddScoped<reportMapper>();
             services.AddScoped<IUsers,UserRepo>();
             services.AddScoped<IImage,ImageImplementation>();
             services.AddScoped<ITokenService,TokenService>();
